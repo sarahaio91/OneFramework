110eafacdfbd61660965acfc7c372ab46c0b6ace
Truong Le <sarahaio91@gmail.com>
2017-08-19 00:41:11 +0700

    Add application insight

--- OneFramework/BackEnd/Web/Program.cs
+++ OneFramework/BackEnd/Web/Program.cs
@@ -12,6 +12,7 @@ namespace Web
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
